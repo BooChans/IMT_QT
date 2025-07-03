3c6ea5c1a8a85ee692bd422ad5463ca7b7775312
BooChans <baochautran1247@gmail.com>
2025-07-03 16:47:33 +0200

    IFTA : Amp/Pha discretization

--- examples/ifmta/ifta.py
+++ examples/ifmta/ifta.py
@@ -8,9 +8,14 @@ Created on Fri Mar  1 09:58:42 2024 under Python 3.11.7
 # 8<--------------------------- Import modules ---------------------------
 
 import numpy as np
+try:
+    from ifmta.tools import Discretization, SoftDiscretization
+    from ifmta.performance_criterias import ComputeEfficiency, ComputeUniformity
+except: 
+    from tools import Discretization, SoftDiscretization
+    from performance_criterias import ComputeEfficiency, ComputeUniformity
 
-from ifmta.tools import Discretization, SoftDiscretization
-from ifmta.performance_criterias import ComputeEfficiency, ComputeUniformity
+import matplotlib.pyplot as plt
 
 # 8<------------------------- Functions definitions ----------------------
 
@@ -293,8 +298,174 @@ def IftaOverCompensation(target, image_size, *, n_iter = 25, compute_efficiency 
 
 
 
+def IftaImproved(target, *, image_size=None, n_iter_ph1=25, n_iter_ph2, rfact=1.2, n_levels=0, compute_efficiency=0, compute_uniformity=0, seed=0):
+
+    """
+    Ifta : Iterative Fourier Transform Algorithm
+    
+    Author : Francois Leroux, mod. Bao Chau Tran
+    Contact : francois.leroux.pro@gmail.com
+    Status : in progress
+    Last update : 2024.03.01, Brest
+    Comments : for even support size, coordinates are defined like [-2,-1,0,1] (N = 4)
+    
+    Inputs : MANDATORY : target {2D float np.array}[Irradiance] : image we want to get at infinity under plane wave illumination  
+                         image_size {tupple (1x2)}[pixel] : size of the image plane 
+                                                            should have larger elements than target_image.shape 
+                                                            is equal to holo_size
+    
+              OPTIONAL :  n_iter : number of iterations of each loop of the algorithm {int} - default value = 25
+                          r_fact : reinforcment factor. Forces the energy to stay in the ROI - default value = 1.2
+                          n_levels : number of levels over which the phase will be discretized 
+                                    default value = 0 : no Discretization
+                          compute_efficiency {bool} : If 1, efficiency is computed and returned along the loop
+                                                      default value = 0
+                          compute_uniformity {bool} : If 1, uniformity is computed and returned along the loop
+                                                      default value = 0
+                        
+    Outputs : a binary cross
+    """
+    if len(target.shape) == 3:
+        target = target.squeeze()
+
+    
+    if compute_efficiency and n_levels == 0:  # memory allocation
+        efficiency = np.zeros(n_iter_ph1)   
+    else: 
+        efficiency = np.zeros(n_iter_ph2)   
+      
+        
+    if compute_uniformity and n_levels == 0:  # memory allocation
+
+        uniformity = np.zeros(n_iter_ph1) 
+    else:
+        uniformity = np.zeros(n_iter_ph2) 
+    
+    target_size = target.shape
+    
+    target_amp = np.asarray(target, float)       # conversion target to float
+    target_amp = np.sqrt(target_amp)             # get target amplitude
+    
+    if image_size == None:
+        image_amp = target_amp
+        image_size = image_amp.shape
+        
+    else:
+        image_amp = np.zeros(image_size)             # Amplitude output field = 0
+        image_amp[image_size[0]//2-target_size[0]//2:image_size[0]//2-target_size[0]//2+target_size[0], 
+                  image_size[1]//2-target_size[1]//2:image_size[1]//2-
+                  target_size[1]//2+target_size[1]] = target_amp   # Amplitude = target image in window
+    
+    
+    if type(seed) == int:
+        image_phase = 2*np.pi*np.random.rand(image_size[0], image_size[1]) # Random image phase
+    else:
+        image_phase = seed
+
+    cont = 0
+    h,w = image_phase.shape
+    if n_levels != 0:
+        shape = (n_iter_ph1 + n_iter_ph2 + 1, h, w)
+    else:
+        shape = (n_iter_ph1 + 1, h, w)
+    holo_phase_fields = np.zeros(shape)
+    holo_phase_fields[cont] = image_phase   
 
 
 
+    image_field = image_amp*np.exp(1j * image_phase)      # Initiate input field
+    
+    # First loop - continous phase screen computation
+    for k in range(n_iter_ph1):
+        cont+=1
+        holo_field = np.fft.ifft2(np.fft.ifftshift(image_field))  # field ifta = TF-1 field image
+        holo_phase = np.angle(holo_field)                         # save ifta phase
+        holo_phase_fields[cont] = holo_phase                      # save holo phase from each iteration
+        holo_field = np.exp(holo_phase * 1j)                      # force the module of holo_field to 1 (no losses)
+        image_field = np.fft.fftshift(np.fft.fft2(holo_field))    # field image = TF field ifta
+        image_phase = np.angle(image_field)                       # save image phase
+        image_amp[image_size[0]//2-target_size[0]//2:image_size[0]//2-target_size[0]//2+target_size[0], 
+                  image_size[1]//2-target_size[1]//2:
+                      image_size[1]//2-
+                      target_size[1]//2+target_size[1]] = rfact*target_amp  # force the amplitude of the ifta 
+                                                                            # to the target amplitude inside the ROI. 
+                                                                            # amplitude freedom : outside the ROI,
+                                                                            # the amplitude is free.
+        image_field = image_amp*np.exp(image_phase * 1j)                    # new image field computation
+        
+        if n_levels ==0 and compute_efficiency:
+            efficiency[k] = ComputeEfficiency(holo_phase, image_amp)
+            
+        if n_levels ==0 and compute_uniformity:
+            uniformity[k] = ComputeUniformity(holo_phase, image_amp)
 
+    # Second loop - discretized phase screen
+
+    if n_levels != 0:
+
+        for k in range(n_iter_ph2):
+            cont += 1
+            holo_field = np.fft.ifft2(np.fft.ifftshift(image_field))  # field ifta = TF-1 field image
+            holo_phase = np.angle(holo_field)                         # get ifta phase. phase values between 0 and 2pi 
+            holo_phase = Discretization(holo_phase, n_levels)         # phase Discretization
+            holo_phase_fields[cont] = holo_phase                     # save holo phase from each iteration
+            holo_field = np.exp(holo_phase * 1j)                      # force the amplitude of the ifta to 1 (no losses)
+            image_field = np.fft.fftshift(np.fft.fft2(holo_field))    # image = TF du ifta
+            image_phase = np.angle(image_field)                       # save image phase
+            image_amp[image_size[0]//2-target_size[0]//2:
+                      image_size[0]//2-target_size[0]//2+target_size[0], 
+                      image_size[1]//2-target_size[1]//2:image_size[1]//2-target_size[1]//2+
+                      target_size[1]] = rfact*target_amp               # force the amplitude of the ifta to the target amplitude 
+                                                                       # inside the ROI. Outside the ROI, the amplitude is free
+            image_field = image_amp*np.exp(image_phase * 1j)           # new image field computation
+            
+            if compute_efficiency:
+                efficiency[k] = ComputeEfficiency(holo_phase, image_amp)
+                
+            if compute_uniformity:
+                uniformity[k] = ComputeUniformity(holo_phase, image_amp)
+
+    if compute_efficiency and not(compute_uniformity):
+        return holo_phase_fields, efficiency
+    
+    if compute_uniformity and not(compute_efficiency):
+        return holo_phase_fields, uniformity
+    
+    if compute_efficiency and compute_uniformity:
+        return holo_phase_fields, efficiency, uniformity
+
+    return holo_phase_fields
+
+
+def AmpDiscretization(image_field, amplitude):
+    image_amp = np.abs(image_field)
+    image_amp = np.where(image_amp <= amplitude, 1, image_amp)
+    return image_amp
+
+def PhaDiscretization(image_field,  phase_levels, delta_phase):
+    segments = [(phase_levels[i]-delta_phase, phase_levels[i]+delta_phase) for i in range(len(phase_levels))]
+    phase = np.angle(image_field)
+    phase = np.mod(phase, 2*np.pi)
+
+    for start, end in segments:
+        mid = (start + end) / 2
+        mask = (phase >= start) & (phase <= end)
+        phase[mask] = mid*
+    
+    return phase
+
+if __name__ == "__main__":
+    shape = (512, 512)
+    h, w = shape
+    image_field = np.ones(shape)
+
+    # Random angle (uniform between 0 and 2π)
+    theta = 2 * np.pi * np.random.rand(10)
+    # Random radius with sqrt to ensure uniform distribution in disk
+    r = np.sqrt(np.random.rand(10))
+    val = r * np.exp(1j * theta)
+    val_crr = AmpDiscretization(val, 0.5)
+    print(np.abs(val))
+    print()
+    print(np.abs(val_crr))
 
